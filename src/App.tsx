4708a0f81d5a022678e3003c75c2ea265206e7da
jnorwood1504 <144853512+jnorwood1504@users.noreply.github.com>
2023-11-26 19:48:38 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import About from "./Views/About";
 import Home from "./Views/Home";
 import Inventory from "./Views/Inventory";
+import ResourceList from ".Views/Inventory";
 
 import React from "react";
 import {
@@ -18,6 +19,7 @@ function App() {
             <Route index element={<Home/>}></Route>
             <Route path="about" element={<About/>}></Route>
             <Route path="inventory" element={<Inventory/>}></Route>
+            <Route path="resourcelist" element={<ResourceList/>}></Route>
           </Route>
         </Routes>
     </Router>
