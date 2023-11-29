72c4e7083114602d0a7ea71f0b3db4274542b603
jnorwood1504 <144853512+jnorwood1504@users.noreply.github.com>
2023-11-28 19:26:24 -0600

    Create ResourceTableConstants.tsx

---
+++ src/DataConstants/ResourceTableConstants.tsx
@@ -0,0 +1,11 @@
+import {ResourceTableRow} from '../DataObjects/ResourceTableInterface';
+//import {INIT_RESULT_DATA} from "../DataConstants/ClientTableRow";
+
+export const INIT_RESULT_RESOURCE_DATA: ResourceTableRow = {
+        id: 0,
+        Resources: " ",
+        ResourcesName: " ",
+        ResourcesAddress: 0,
+        ResourcesCurrentNum: 0,
+        ResourceMaxNum: 0
+    };
