f51358686394ddc0be5ea984f73a9d4e54e56e4e
jnorwood1504 <144853512+jnorwood1504@users.noreply.github.com>
2023-11-29 18:04:32 -0600

    Create Help.jsx

---
+++ src/Views/Help.jsx
@@ -0,0 +1,61 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("../App.css");
+require("bulma/css/bulma.min.css");
+function Search() {
+    return (
+    // <div className="App">
+    //   <div className="container">
+    //     <div className="columns">
+    //     <div className="column">
+    //     <h1 className="title">
+    //           Recent Changes
+    //         </h1>
+    //         </div>
+    //     </div>
+    //   </div>
+    //   <div className="container2">
+    //     <div className="columns is-centered">
+    //       <div className="column is-half">
+    //       <button className="button">
+    //         Show more recent changes
+    //       </button>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="content container has-text-centered">
+        <h1 className="title has-text-success">
+          Welcome
+        </h1>
+          <h1 className="subtitle">
+            To The Help Page
+          </h1>
+        <div className="tile is-ancestor">
+          <div className="tile is-parent is-20">
+            <article className="tile is-child has-background-dark box">
+              <h1 className="title is-underlined has-text-white"> Sorry Our Help Desk is Down as of Right Now </h1>
+              
+              <h1>The company itself is a very successful company. May the pains be pleased, and will he criticize them, the consequences of things, and leave them in error; are they in reason, unless there are times of blissful resilience, and those are often harsher to the inventor?
+              I will explain the ways to take pleasure in our lives? He hates to be repulsed, and we can't help him. At times, these things? Flee from pleasure itself, for it will result in less caressing, to be accepted by labors, happy, from, therefore, or itself repulsed. No one likes those who, with even the smallest degree of flexibility, are exempted from the necessities of labor.</h1>
+              <button>Request Help</button>
+            </article>
+            {/* <article className="tile is-child box has-background-warning">
+          <p className="title"> coding </p>
+        </article>
+        <article className="tile is-child box has-background-warning">
+          <p className="title"> coding </p>
+        </article> */}
+          </div>
+        </div>
+        {/* <div className="tile is-ancestor">
+          <div className="tile is-parent">
+            <div className="tile is-child box has-background-warning">
+              <p className="title"> coding </p>
+            </div>
+          </div>
+        </div> */}
+
+      </div>);
+}
+exports.default = Search;
